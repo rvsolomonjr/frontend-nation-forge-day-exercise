72fb66308c0a740c19c8baf63b86f16e47e1cb8d
James Swinton-Bland <james_swinton@hotmail.co.uk>
2025-06-03 16:07:55 +0100

    Update starting point template

--- src/components/DataGrid.tsx
+++ src/components/DataGrid.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useRef, useState } from 'react';
+import React, { useCallback, useMemo } from 'react';
 
 // Types
 import { DataGridProps, Ticker } from '../utils/types';
@@ -9,16 +9,17 @@ import {
   CellClassParams,
   ColDef,
   FirstDataRenderedEvent,
-  RowNodeSelectedEvent,
+  GetRowIdParams,
   RowSelectionOptions,
+  SelectionChangedEvent,
   ValueFormatterParams,
   ValueGetterParams,
 } from 'ag-grid-community';
 
 // Cell Renderers
-import { TickerCellRenderer } from './TickerCellRenderer';
+import TickerCellRenderer from './TickerCellRenderer';
 
-// Default Col Def
+// Default Col Def (Applies to All Columns)
 const defaultColDef = {
   filter: true,
   flex: 1,
@@ -26,17 +27,37 @@ const defaultColDef = {
 
 // Currency Value Formatter
 const currencyFormatter = (params: ValueFormatterParams): string => {
-  // TODO: Implement currency formatter
+  // TODO: Implement currency formatter (Step 2)
 };
 
 // Profit And Loss Value Getter
 const calculateProfitAndLoss = (params: ValueGetterParams) => {
-  // TODO: Implement profit and loss calculation
+  // TODO: Implement profit and loss calculation (Step 5)
 };
 
 // Profit And Loss Cell Style
 const getProfitAndLossCellStyle = (params: CellClassParams) => {
-  // TODO: Implement conditional cell styles for Profit & Loss Column
+  // TODO: Implement conditional cell styles for Profit & Loss Column (Step 6)
+};
+
+// Row Selection Options
+const rowSelection = {
+  // TODO: Implement Row Selection Strategy (Step 7)
+} as RowSelectionOptions;
+
+// On First Data Rendered Event
+const onFirstDataRendered = (params: FirstDataRenderedEvent) => {
+  // TODO:  Create Integrated TreeMap Chart on Load (Step 9)
+};
+
+// Create Integrated TreeMap Chart
+const createIntegratedTreeMapChart = (params: FirstDataRenderedEvent) => {
+  // TODO: Implement integrated treeMap chart creation (Step 9)
+};
+
+// Set Row ID Strategy
+const getRowId = (params: GetRowIdParams): string => {
+  // TODO: Implement Row ID strategy to uniquely identify each row (Step 10)
 };
 
 const DataGrid: React.FC<DataGridProps> = ({ data = [], setSelectedRow }) => {
@@ -64,25 +85,9 @@ const DataGrid: React.FC<DataGridProps> = ({ data = [], setSelectedRow }) => {
     ];
   }, []);
 
-  // Row Selection Options
-  const rowSelection = useMemo<RowSelectionOptions>(
-    () => ({
-      // TODO: Implement Row Selection Strategy
-    }),
-    []
-  );
-
-  // On Row Selection Event Handler
-  const onRowSelected = useCallback(
-    (event: RowNodeSelectedEvent) => {
-      // TODO: Implement Row Selection Event Handler to pass data to parent component
-    },
-    [setSelectedRow]
-  );
-
-  // On First Data Rendered Event
-  const onFirstDataRendered = useCallback((params: FirstDataRenderedEvent) => {
-    // TODO:  Create Integrated Treemap Chart on Load
+  // Selection Changed Event Handler
+  const onSelectionChanged = useCallback((event: SelectionChangedEvent) => {
+    // TODO: Implement Selection Changed Event Handler to get currently selected row and pass its data to parent component (Step 7)
   }, []);
 
   return (
